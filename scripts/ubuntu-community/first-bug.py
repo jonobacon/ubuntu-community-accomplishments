eb1b38c169dfcb998d9e9ab3daad7324ef01b7bf
Jono Bacon <jono@ubuntu.com>
2012-01-31 19:01:10 -0800

    Upgraded the accomplishment for the new validation approach.

--- scripts/ubuntu-community/first-bug.py
+++ scripts/ubuntu-community/first-bug.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 
-import traceback
+import traceback, sys
+
+sys.path.append("/home/jono/source/trophyinfo/daemon")
+import libaccomplishments
+
 try:
     import json, sys, os, pwd, subprocess
     from ubuntuone.couch import auth
     from launchpadlib.launchpad import Launchpad
 
     # Get user's email from Ubuntu One, since we have U1 credentials
-    email = json.loads(auth.request(url='https://one.ubuntu.com/api/account/')[1])['email']
+    #email = json.loads(auth.request(url='https://one.ubuntu.com/api/account/')[1])['email']
+    #email = "jono@ubuntu.com"
+
+    libaccom = libaccomplishments.Accomplishments()
+    f = libaccom.getExtraInformation("ubuntu-community", "launchpad-email")
+    print 
+    if bool(f[0]["launchpad-email"]) == False:
+        sys.exit(4)
+    else:
+        email = f[0]["launchpad-email"]
 
     # Get count of bugs reported by user from Launchpad, using email to identify
     l=Launchpad.login_anonymously('ubuntu-community accomplishments','production')
