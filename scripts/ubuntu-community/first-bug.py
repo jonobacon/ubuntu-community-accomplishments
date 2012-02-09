6bc31de58e088a02594b6c6db29984d25f7c92ea
Jono Bacon <jono@ubuntu.com>
2012-02-09 15:26:36 -0800

    Fixed accomplishment script. Now works correctly.

--- scripts/ubuntu-community/first-bug.py
+++ scripts/ubuntu-community/first-bug.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 import traceback, sys
-
-sys.path.append("/home/jono/source/trophyinfo/daemon")
 import libaccomplishments
 
 try:
@@ -10,13 +8,10 @@ try:
     from ubuntuone.couch import auth
     from launchpadlib.launchpad import Launchpad
 
-    # Get user's email from Ubuntu One, since we have U1 credentials
-    #email = json.loads(auth.request(url='https://one.ubuntu.com/api/account/')[1])['email']
-    #email = "jono@ubuntu.com"
-
     libaccom = libaccomplishments.Accomplishments()
+
     f = libaccom.getExtraInformation("ubuntu-community", "Launchpad Email")
-    print 
+
     if bool(f[0]["Launchpad Email"]) == False:
         sys.exit(4)
     else:
@@ -25,14 +20,22 @@ try:
     # Get count of bugs reported by user from Launchpad, using email to identify
     l=Launchpad.login_anonymously('ubuntu-community accomplishments','production')
     me=l.people.getByEmail(email=email)
-    ubuntu=l.projects['ubuntu']
-    bugs_reported = ubuntu.searchTasks(bug_reporter=me, 
-        status=['New', 'Incomplete', 'Invalid', 'Confirmed', 'Triaged', 
-        'In Progress', 'Fix Committed', 'Fix Released', 'Opinion', "Won't Fix"])
-    if len(bugs_reported) > 0:
-        sys.exit(0)
-    else:
+
+    if me == None:
         sys.exit(1)
+    else:
+        ubuntu=l.projects['ubuntu']
+        bugs_reported = ubuntu.searchTasks(bug_reporter=me, 
+            status=['New', 'Incomplete', 'Invalid', 'Confirmed', 'Triaged', 
+            'In Progress', 'Fix Committed', 'Fix Released', 'Opinion', "Won't Fix"])
+
+        print len(bugs_reported)
+
+        if len(bugs_reported) > 0:
+            sys.exit(0)
+        else:
+            sys.exit(1)
+
 except SystemExit, e:
     sys.exit(e.code)
 except:
