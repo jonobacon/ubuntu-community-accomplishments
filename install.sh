6c717b890e699bdde032ccb243b6360115c80a0a
Jono Bacon <jono@ubuntu.com>
2012-02-09 20:08:14 -0800

    Fixed installation script.

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "Copying accomplishments to $1"
 rm -rf $1/accomplishments/ubuntu-community
-cp -r ./accomplishments/ubuntu-community $1/accomplishments/
+mkdir $1/accomplishments/ubuntu-community
+cp -r ./accomplishments/ubuntu-community/* $1/accomplishments/ubuntu-community/
 
 rm -rf $1/scripts/ubuntu-community
-cp -r ./scripts/ubuntu-community $1/scripts/
+mkdir $1/scripts/ubuntu-community
+cp -r ./scripts/ubuntu-community $1/scripts/ubuntu-community/
 echo "Done!"
