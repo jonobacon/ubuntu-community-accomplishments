7677d15708cdef29b91e4702ed6499bb5b087838
Jono Bacon <jono@ubuntu.com>
2012-02-22 22:57:16 -0800

    Added LoCo Team member accomplishment.

---
+++ scripts/ubuntu-community/member-loco-team.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import traceback, sys
+import libaccomplishments
+
+try:
+    import json, sys, os, pwd, subprocess
+    from ubuntuone.couch import auth
+    from launchpadlib.launchpad import Launchpad
+
+    libaccom = libaccomplishments.Accomplishments()
+
+    f = libaccom.getExtraInformation("ubuntu-community", "launchpad-email")
+
+    if bool(f[0]["launchpad-email"]) == False:
+        sys.exit(2)
+    else:
+        email = f[0]["launchpad-email"]
+
+    lp=Launchpad.login_anonymously('ubuntu-community accomplishments','production')
+    me=lp.people.getByEmail(email=email)
+
+    if me == None:
+        sys.exit(1)
+    else:
+        user = me.name
+        final = []
+        for team in me.super_teams:
+            for sup in team.super_teams:
+                if "locoteams" in sup.self_link.rsplit('~', 1)[-1]:
+                    final.append(sup)
+
+        if len(final) is not 0:
+            sys.exit(0)
+        else:
+            sys.exit(1)
+            
+except SystemExit, e:
+    sys.exit(e.code)
+except:
+    traceback.print_exc()
+    sys.exit(2)
+
