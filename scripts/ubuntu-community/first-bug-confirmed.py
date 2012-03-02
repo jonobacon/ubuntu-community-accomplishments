0e6e67b6944162512da3c2ca8fcc0c7b1db32e0f
Bruno Girin <brunogirin@gmail.com>
2012-03-02 14:14:47 +0000

    Added a new accomplishment for users who filed a big that was subsequently confirmed.

---
+++ scripts/ubuntu-community/first-bug-confirmed.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import traceback, sys
+import libaccomplishments
+
+try:
+    import json, sys, os, pwd, subprocess
+    from ubuntuone.couch import auth
+    from launchpadlib.launchpad import Launchpad
+
+    libaccom = libaccomplishments.Accomplishments()
+
+    f = libaccom.getExtraInformation("ubuntu-community", "launchpad-email")
+
+    if bool(f[0]["launchpad-email"]) == False:
+        sys.exit(4)
+    else:
+        email = f[0]["launchpad-email"]
+
+    # Get count of bugs reported by user from Launchpad, using email to identify
+    l=Launchpad.login_anonymously('ubuntu-community accomplishments','production')
+    me=l.people.getByEmail(email=email)
+
+    if me == None:
+        sys.exit(1)
+    else:
+        ubuntu=l.projects['ubuntu']
+        bugs_reported = ubuntu.searchTasks(bug_reporter=me, 
+            status=['Confirmed', 'Triaged', 
+            'In Progress', 'Fix Committed', 'Fix Released'])
+
+        if len(bugs_reported) > 0:
+            sys.exit(0)
+        else:
+            sys.exit(1)
+
+except SystemExit, e:
+    sys.exit(e.code)
+except:
+    traceback.print_exc()
+    sys.exit(2)
+
