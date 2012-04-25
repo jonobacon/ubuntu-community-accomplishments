745137f29a9e585c65955976764695dae4138407
Rafał Cieślak <rafalcieslak256@gmail.com>
2012-04-25 22:40:15 +0200

    fix for missing needs-information in localized accomplishments

--- generatelocalizedaccomplishments.py
+++ generatelocalizedaccomplishments.py
@@ -47,6 +47,9 @@ class GenerateTranslations():
                 if self.masterconfig.has_option("accomplishment", "needs-signing"):
                     self.outputconfig.set("accomplishment", "needs-signing", self.masterconfig.get("accomplishment", "needs-signing"))                
                 
+                if self.masterconfig.has_option("accomplishment", "needs-information"):
+                    self.outputconfig.set("accomplishment", "needs-information", self.masterconfig.get("accomplishment", "needs-information")) 
+                    
                 if self.masterconfig.has_option("accomplishment", "icon"):
                     self.outputconfig.set("accomplishment", "icon", self.masterconfig.get("accomplishment", "icon"))                
                 
