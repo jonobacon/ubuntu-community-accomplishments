5748b0a87e218a3cb463da303b9ef71cb6b06128
angeloc <angelo.compagnucci@gmail.com>
2012-04-26 11:09:08 +0200

    Accomplishment to check if a user can have branches merged (member of contributor-agreement-canonical team)
    added:
      accomplishments/ubuntu-community/en/canonical-contributor.accomplishment
      scripts/ubuntu-community/canonical-contributor.py

---
+++ scripts/ubuntu-community/canonical-contributor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import traceback, sys
+
+from accomplishments.daemon import dbusapi
+
+
+try:
+    import sys, os, pwd, subprocess
+    from launchpadlib.launchpad import Launchpad
+
+    api = dbusapi.Accomplishments()
+    f = api.get_extra_information("ubuntu-community", "launchpad-email")
+    if bool(f[0]["launchpad-email"]) == False:
+        sys.exit(4)
+    else:
+        email = f[0]["launchpad-email"]
+
+    # Get count of bugs reported by user from Launchpad, using email to
+    # identify
+    l = Launchpad.login_anonymously(
+        'ubuntu-community accomplishments', 'production')
+    me = l.people.getByEmail(email=email)
+    if me == None:
+        sys.exit(1)
+    else:
+        team = l.people['contributor-agreement-canonical']
+        members = team.participants
+        for member in members:
+          if member.name == me.name:
+            sys.exit(0)
+        sys.exit(1)
+
+except SystemExit, e:
+    sys.exit(e.code)
+except:
+    traceback.print_exc()
+    sys.exit(2)
