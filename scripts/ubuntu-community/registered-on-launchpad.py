6c479a69495eeeedb3598111bc9bbab889d9bedc
angeloc <angelo.compagnucci@gmail.com>
2012-04-27 16:46:41 +0200

    Fixing double imports in scripts, moving other imports to the top to make it consistent.
    modified:
      accomplishments/ubuntu-community/en/first-branch-merged.accomplishment
        Fixed a typo
      scripts/ubuntu-community/*.py
        Fixing double imports in scripts, moving other imports to the top to make it consistent.

--- scripts/ubuntu-community/registered-on-launchpad.py
+++ scripts/ubuntu-community/registered-on-launchpad.py
@@ -2,19 +2,16 @@
 import traceback, sys
 
 from accomplishments.daemon import dbusapi
-
+from launchpadlib.launchpad import Launchpadbzr
 
 try:
-    import sys, os, pwd, subprocess
-    from launchpadlib.launchpad import Launchpad
-
     api = dbusapi.Accomplishments()
     f = api.get_extra_information("ubuntu-community", "launchpad-email")
     if bool(f[0]["launchpad-email"]) == False:
         sys.exit(4)
     else:
         email = f[0]["launchpad-email"]
-    # Get count of bugs reported by user from Launchpad, using email to
+    # Get count of bugs repogeanrted by user from Launchpad, using email to
     # identify
     l = Launchpad.login_anonymously(
         'ubuntu-community accomplishments', 'production')
