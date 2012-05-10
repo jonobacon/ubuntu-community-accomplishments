33ad41f194a5bf99bba0a4199fedcd59dc2610be
Rafał Cieślak <rafalcieslak256@gmail.com>
2012-05-10 20:36:48 +0200

    Synchronised translations.

---
+++ generated/accomplishments/forum-staff-member.c
@@ -0,0 +1,79 @@
+[accomplishment]
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('title' field)
+// .
+// ENGLISH TRANSLATION:
+// Ubuntu Forums Staff Member
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// A short description of the accomplishment.
+//          NOTE: Describe this in the past tense as if it has been achieved (e.g. Registered On Launchpad). 
+_("forum-staff-member_title")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('description' field)
+// .
+// ENGLISH TRANSLATION:
+// You are a member of the Ubuntu Forums Staff
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a descriptive single-line summary of the accomplishment.
+_("forum-staff-member_description")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('summary' field)
+// .
+// ENGLISH TRANSLATION:
+// The Ubuntu Forums Staff are the moderation team behind the Ubuntu Forums.  The Ubuntu Forums staff tackle day to day running of the forum and gives feedback to the Forum Council when requested.
+// Members of the Forum Staff have demonstrated a strong commitment and participation history in Ubuntu, clear leadership skills, and have the time and availability to commit to the needs of the forums. Members are also expected to have good people skills and have the ability to empathise with others.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Introduce the accomplishment, explain what the different concepts are that are involved, and provide guidance on how to accomplish it.
+//          NOTE: Break this into paragraphs by putting each paragraph on a new line. 
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("forum-staff-member_summary")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('steps' field)
+// .
+// ENGLISH TRANSLATION:
+// Become an Ubuntu Forums Member.
+// Wait for elections to be held on new staff. The Forum Council will make a shortlist of candidates, if you are included in the shortlist you will be contacted inviting you to stand for election.
+// If you agree to stand the Ubuntu Forums Members will have the opportunity to vote for you.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a series of step-by-step instructions for how to accomplish this trophy.
+//          NOTE: Put each step on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("forum-staff-member_steps")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('tips' field)
+// .
+// ENGLISH TRANSLATION:
+// Focus on your soft skills, having good people skills is very important.
+// Be active in the support areas of the forum, but also be seen in the Community Cafe
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add tips and best practise for accomplishing this trophy.
+//          NOTE: Put each tip on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("forum-staff-member_tips")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('pitfalls' field)
+// .
+// ENGLISH TRANSLATION:
+// Please do not ask to become Forums Staff
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add things the user should not do when working to accomplish this trophy.
+//          NOTE: Put each pitfall on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("forum-staff-member_pitfalls")
+// ACCOMPLISHMENT: Ubuntu Forums Staff Member ('help' field)
+// .
+// ENGLISH TRANSLATION:
+// #ubuntuforums on Freenode
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add related help resources (e.g. IRC channel names).
+//          NOTE: Put each help resource on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("forum-staff-member_help")
