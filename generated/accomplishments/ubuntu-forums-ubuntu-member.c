598ed2e47a68032997c7d80d1a0896acbb0f2517
Rafał Cieślak <rafalcieslak256@gmail.com>
2012-05-11 11:41:21 +0200

    * Translation sync

---
+++ generated/accomplishments/ubuntu-forums-ubuntu-member.c
@@ -0,0 +1,92 @@
+[accomplishment]
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('title' field)
+// .
+// ENGLISH TRANSLATION:
+// Ubuntu Forums Ubuntu Member
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// A short description of the accomplishment.
+//          NOTE: Describe this in the past tense as if it has been achieved (e.g. Registered On Launchpad). 
+_("ubuntu-forums-ubuntu-member_title")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('description' field)
+// .
+// ENGLISH TRANSLATION:
+// You gained Ubuntu Membership via Forums Contributions
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a descriptive single-line summary of the accomplishment.
+_("ubuntu-forums-ubuntu-member_description")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('summary' field)
+// .
+// ENGLISH TRANSLATION:
+// Ubuntu membership generally recognizes significant and sustained contributions to Ubuntu. An active Forums contributor is held to the same standards, but the main focus of activity is in the Ubuntu Forums.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Introduce the accomplishment, explain what the different concepts are that are involved, and provide guidance on how to accomplish it.
+//          NOTE: Break this into paragraphs by putting each paragraph on a new line. 
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-forums-ubuntu-member_summary")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('steps' field)
+// .
+// ENGLISH TRANSLATION:
+// Create a <a href="https://wiki.ubuntu.com/username">wiki page</a>.
+// Create a <a href="https://launchpad.net">Launchpad</a> page. Although not mandatory, many people use the same identity/nick on Launchpad and on the forums.
+// Sign the Ubuntu code of conduct. This has traditionally been one of the more difficult steps. FYI: A bug has been opened on Launchpad to make the process easier.
+// Create a thread in the <a href="http://ubuntuforums.org/forumdisplay.php?f=404">Ubuntu Membership Applications section</a> requesting a review of your application. Be sure to include a link to your wiki and launchpad pages.
+// We would strongly advise you read through <a href="http://ubuntuforums.org/showthread.php?t=1750642">this</a> thread before applying for membership.
+// The Forum Council meets monthly and membership applications are processed at this time. For information on when the Forum Council meet please check <a href="https://wiki.ubuntu.com/ForumCouncilAgenda">this</a> wiki page.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a series of step-by-step instructions for how to accomplish this trophy.
+//          NOTE: Put each step on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-forums-ubuntu-member_steps")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('tips' field)
+// .
+// ENGLISH TRANSLATION:
+// Focus on helping other forum members in the Absolute Beginners Talk and General Help. Reporting Spam is another good way to make a positive contribution to the forum.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add tips and best practise for accomplishing this trophy.
+//          NOTE: Put each tip on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-forums-ubuntu-member_tips")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('pitfalls' field)
+// .
+// ENGLISH TRANSLATION:
+// Do not apply without having at least 6 months of sustained forums contributions. Ideally you would have 12.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add things the user should not do when working to accomplish this trophy.
+//          NOTE: Put each pitfall on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-forums-ubuntu-member_pitfalls")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('links' field)
+// .
+// ENGLISH TRANSLATION:
+// http://ubuntuforums.org/showthread.php?t=1750642
+// https://wiki.ubuntu.com/ForumCouncilAgenda
+// https://wiki.ubuntu.com/Forums/Membership
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add related web addresses (don't include a HTML link).
+//          NOTE: Put each URL on a new line
+_("ubuntu-forums-ubuntu-member_links")
+// ACCOMPLISHMENT: Ubuntu Forums Ubuntu Member ('help' field)
+// .
+// ENGLISH TRANSLATION:
+// #ubuntuforums on Freenode
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add related help resources (e.g. IRC channel names).
+//          NOTE: Put each help resource on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-forums-ubuntu-member_help")
