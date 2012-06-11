4cb1c7201152d30d6f7440774c91491414128393
Rafał Cieślak <rafalcieslak256@gmail.com>
2012-06-11 21:17:19 +0200

    * Translations sync

---
+++ generated/accomplishments/ubuntu-community/registered-on-askubuntu.c
@@ -0,0 +1,71 @@
+[accomplishment]
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('title' field)
+// .
+// ORIGINAL TRANSLATION:
+// Register on Ask Ubuntu
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// A short description of the accomplishment.
+//          NOTE: Describe this in the past tense as if it has been achieved (e.g. Registered On Launchpad). 
+_("ubuntu-community/registered-on-askubuntu_title")
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('description' field)
+// .
+// ORIGINAL TRANSLATION:
+// You have registered on Ask Ubuntu
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a descriptive single-line summary of the accomplishment.
+_("ubuntu-community/registered-on-askubuntu_description")
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('summary' field)
+// .
+// ORIGINAL TRANSLATION:
+// <a href="http://askubuntu.com">Ask Ubuntu</a> is a web site where users can ask their questions related to Ubuntu. It is a great way of requesting support, it's easy and quick, and one does not need to register there to ask something. However, users who want to propose answers and use all functions of the site, including comments, voting, and many more, need to register an account.
+// This accomplishment unlocks all other trophies recieved by activity on Ask Ubuntu.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Introduce the accomplishment, explain what the different concepts are that are involved, and provide guidance on how to accomplish it.
+//          NOTE: Break this into paragraphs by putting each paragraph on a new line. 
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-community/registered-on-askubuntu_summary")
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('steps' field)
+// .
+// ORIGINAL TRANSLATION:
+// Go to <a href="http://askubuntu.com">http://askubuntu.com</a>.
+// Click the <i>log in</i> link on site's top panel.
+// If you have any of listed accounts, you can use it to login. Otherwise, choose to sign up, this will create you a StackExchange account you can then use to log in.
+// Provide data you are asked for.
+// Once you are logged in, click your username on site's top panel. Copy the address of the site you will be taken to.
+// In this very application, select <i>Edit->Identification</i> menu, and paste that address in apropriate field. This will notify the Accomplishments System of your account.
+// Once your account is verified, you will recive this trophy.
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add a series of step-by-step instructions for how to accomplish this trophy.
+//          NOTE: Put each step on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-community/registered-on-askubuntu_steps")
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('links' field)
+// .
+// ORIGINAL TRANSLATION:
+// http://www.askubuntu.com
+// http://meta.askubuntu.com
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add related web addresses (don't include a HTML link).
+//          NOTE: Put each URL on a new line
+_("ubuntu-community/registered-on-askubuntu_links")
+// ACCOMPLISHMENT: Register on Ask Ubuntu ('help' field)
+// .
+// ORIGINAL TRANSLATION:
+// #ubuntu-stack on Freenode
+
+// .
+// ----- TRANSLATION INSTRUCTIONS ----- 
+// Add related help resources (e.g. IRC channel names).
+//          NOTE: Put each help resource on a new line
+//          FORMATTING ALLOWED: <i> <strong> <tt>
+_("ubuntu-community/registered-on-askubuntu_help")
