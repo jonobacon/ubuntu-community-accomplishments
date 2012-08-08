9546974a0b81425fc38dfe3837dd7c5cff481b47
José Antonio Rey <joseeantonior@joseeantonior.no-ip.org>
2012-08-08 01:12:30 -0500

    Added Ubuntu Youth Membership accomplishment.

---
+++ scripts/ubuntu-community/general/ubuntu-youth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import traceback, sys
+
+from accomplishments.daemon import dbusapi
+# Add scripts/lib/ to the PYTHONPATH
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
+from helpers import Launchpad
+
+try:
+    api = dbusapi.Accomplishments()
+    f = api.get_extra_information("ubuntu-community", "launchpad-email")
+    if bool(f[0]["launchpad-email"]) == False:
+        sys.exit(4)
+    else:
+        email = f[0]["launchpad-email"]
+        
+    me = Launchpad.fetch(email)
+    if "ubuntu-youth" in me.super_teams:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+except SystemExit, e:
+    sys.exit(e.code)
+except:
+    traceback.print_exc()
+    sys.exit(2)
