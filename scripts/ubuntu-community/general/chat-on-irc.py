b8ee87dd9bed250cea984ae44a31905341148fed
Matthew Fischer <matthew.fischer@canonical.com>
2012-12-16 15:58:14 -0700

    fixing up the initial file check and re-spacing stuff

--- scripts/ubuntu-community/general/chat-on-irc.py
+++ scripts/ubuntu-community/general/chat-on-irc.py
@@ -10,18 +10,24 @@ if bool(f[0]["irc-nickname"]) == False:
 else:
     nickname = f[0]["irc-nickname"]
 
-#We only want to check once every hour, when logs are updated. Checking last timestamp, if any exists...
+#We only want to check once every hour, when logs are updated. Checking 
+#last timestamp, if any exists...
 fd = open(os.path.join(os.path.expanduser("~") + "/.cache/accomplishments/", "chatonirc"), "r")
-content = fd.read()
-fd.close()
-if len(content) > 0: #if no time stamp logged
-	timestamp = float(content) +3600
-	if not nickname == "ubottu" and not nickname == "failfailfail" and timestamp > time.time(): #Temporary fix to ensure being able to use accomplishments-battery
-		print "Skipping Ubuntu IRC Chatter accomplishment this time."
-		sys.exit(1)
+if fd:
+    content = fd.read()
+    fd.close()
+    #if no time stamp logged
+    if len(content) > 0:
+        timestamp = float(content) +3600
+        #Temporary hack to ensure being able to use accomplishments-battery
+        if not nickname == "ubottu" and not nickname == "failfailfail"\
+            and timestamp > time.time():
+            print "Skipping Ubuntu IRC Chatter accomplishment this time."
+            sys.exit(1)
 
 #Writing new timestamp
-fd = open(os.path.join(os.path.expanduser("~") + "/.cache/accomplishments/", "chatonirc"), "w")
+fd = open(os.path.join(os.path.expanduser("~") + "/.cache/accomplishments/",\
+    "chatonirc"), "w")
 fd.write(str(time.time()))
 fd.close()
 
@@ -32,49 +38,59 @@ today_month = str(now.month)
 today_day = str(now.day)
 
 if len(today_day) < 2:
-	today_day = '0' + today_day
+    today_day = '0' + today_day
 
 pagelist = []
 #Use the channel list of today to find the valid channels
 try:
-	pageurl = "http://irclogs.ubuntu.com/"+today_year+"/"+today_month+"/"+today_day
-	channelpage = urllib2.urlopen(pageurl)
-	pagelist = channelpage.readlines()
-	channelpage.close()
+    pageurl = "http://irclogs.ubuntu.com/"+today_year+"/"+today_month+"/"+today_day
+    channelpage = urllib2.urlopen(pageurl)
+    pagelist = channelpage.readlines()
+    channelpage.close()
 except Exception, e:
-	print "Error on loading channel list: " + str(e) #The log file for today might not yet exist. We will check again later.
+    #The log file for today might not yet exist. We will check again later.
+    print "Error on loading channel list: " + str(e) 
 
 channellist = []
 
 for line in pagelist:
-	indexno = line.find('#')+1
-	indexdot = line.find('.txt',indexno)
-	if indexno is not -1 and indexdot != -1:
-		channellist.append(line[indexno:indexdot])
+    indexno = line.find('#')+1
+    indexdot = line.find('.txt',indexno)
+    if indexno is not -1 and indexdot != -1:
+        channellist.append(line[indexno:indexdot])
 
 search_string = "] <" + nickname
 
 total_count_result = 0
 
 for irc_channel in channellist:
-	today_web_page = "http://irclogs.ubuntu.com/"+today_year+"/"+today_month+"/"+today_day+"/%23"+irc_channel+".txt"	#build dynamic txt file link from vars
-	today_count_result = 0
-	try:
-		today_response = urllib2.urlopen(today_web_page)	
-		today_page_source = today_response.read()   			#this variable now contains the entire txt file
-		encoding = chardet.detect(today_page_source)
-		if not isinstance(type(encoding['encoding']), str):
-			 encoding['encoding'] = 'utf-8'
-		tekst = today_page_source.decode(encoding['encoding'], "ignore")
-		today_count_result = tekst.count(search_string)  	#count number of times user spoke today
-		today_response.close()
-		
-	except Exception, e:
-		print "Error on channel " + irc_channel + ": " + str(e)#maybe someone wrote a strange character. It doesn't matter. We can skip that line or file.
+    #build dynamic txt file link from vars
+    today_web_page = "http://irclogs.ubuntu.com/"+today_year+"/"+today_month+"/"+today_day+"/%23"+irc_channel+".txt"
+    today_count_result = 0
+    try:
+        today_response = urllib2.urlopen(today_web_page)
+        #this variable now contains the entire txt file
+        today_page_source = today_response.read()
+        encoding = chardet.detect(today_page_source)
+        if not isinstance(type(encoding['encoding']), str):
+                    encoding['encoding'] = 'utf-8'
+        tekst = today_page_source.decode(encoding['encoding'], "ignore")
+        #count number of times user spoke today
+        today_count_result = tekst.count(search_string)
+        today_response.close()
 
-	total_count_result += today_count_result		#get grand total
+    except Exception, e:
+        #maybe someone wrote a strange character. It doesn't matter.
+        #We can skip that line or file.
+        print "Error on channel " + irc_channel + ": " + str(e)
 
-	if total_count_result > 2:    				#we want to see if user has been chatting (said more than two lines)
-		sys.exit(0)
+    #get grand total
+    total_count_result += today_count_result
+
+    #we want to see if user has been chatting (said more than two lines)
+    if total_count_result > 2:
+        sys.exit(0)
 else:
-	sys.exit(1)	#We reached the last file without gathering enough lines. Accomplishment not yet fulfilled.
+    #We reached the last file without gathering enough lines.
+    #Accomplishment not yet fulfilled.
+    sys.exit(1)
